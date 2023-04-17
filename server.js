df9a163bf4d5927b22439c43d4493f60bc646b90
dev-rafa1707 <rafa1707@gmail.com>
2023-04-17 19:44:17 -0300

    add server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+// const routes = require('./routes/routes')
+
+const server = express()
+server.use(express.json())
+server.use(express.urlencoded({extended:true}))
+// server.use(routes)
+
+const PORT = 3000
+
+server.listen(PORT,()=>{
+    console.log('Listening on port ' + PORT)
+})
